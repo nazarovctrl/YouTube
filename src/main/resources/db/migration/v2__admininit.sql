afc07992d5ad1f019e2c30e4420825f309163732
thisiscolinmorgan <thisiscolinmorgan@gmail.com>
2022-12-22 19:13:09 +0500

    Tag

--- src/main/resources/db/migration/v2__admininit.sql
+++ src/main/resources/db/migration/v2__admininit.sql
@@ -1,2 +1,2 @@
-insert into profile (id, name, surname, email, password, status, role, visible, created_date)
-values (1, 'adminjon', 'adminov', 'adminceek@gmail.com', 'Grechka4kg', 'ACTIVE', 'ROLE_ADMIN', true, current_timestamp);
+insert into profile (name, surname, email, password, status, role, visible, created_date)
+values ('adminjon', 'adminov', 'adminceek@gmail.com', '58e7dbf8dc93865be732403e79fbd4eb', 'ACTIVE', 'ROLE_ADMIN', true, current_timestamp);
