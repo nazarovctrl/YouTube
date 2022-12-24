80589fa336b04e1f2969c51afdc1bd77ae6e327a
Nazarov2 <nazarovazimjon888@gmail.com>
2022-12-24 22:59:49 +0500

    video updated

--- src/main/java/com/example/youtube/dto/video/VideoShortInfo.java
+++ src/main/java/com/example/youtube/dto/video/VideoShortInfo.java
@@ -1,4 +1,4 @@
-package com.example.youtube.dto;
+package com.example.youtube.dto.video;
 
 import com.example.youtube.dto.attach.PreviewAttachDTO;
 import com.example.youtube.dto.channel.ChannelShortDTO;
